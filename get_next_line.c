d8463c5faa67da8880dd88a9a89c75a8b0181bd6
José García <gran.pepe@gmail.com>
2021-11-19 17:47:14 +0100

    recomenzando con pruebas pequeñas, hay un free sin malloc

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/29 08:06:13 by josgarci          #+#    #+#             */
-/*   Updated: 2021/11/19 15:27:06 by josgarci         ###   ########.fr       */
+/*   Created: 2021/11/19 16:46:54 by josgarci          #+#    #+#             */
+/*   Updated: 2021/11/19 17:46:36 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,24 @@
 //#define BUFFER_SIZE 1
 //#define LINEAS 0
 
-char	*ft_split_line(char **rest,int first_n);
-char	*ft_readtext(int fd, int *first_n, char **rest);
+char    *ft_split_line(char **rest,int first_n);
+char    *ft_readtext(int fd, int *first_n, char **rest);
 
 char	*get_next_line(int fd)
 {
 	static char	*rest;
 	int			first_n;
 
-	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd,0,0) == -1)
+	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, 0, 0) == -1)
 		return (0);
-	first_n = ft_strchr(rest, 10);
-	if (!ft_readtext(fd, &first_n, &rest))
-	{
-		if (rest)
-			free(rest);
-		return (0);
-	}
-	else
-	{
-		return(ft_split_line(&rest, first_n));
-	}
+	first_n = ft_strchr (rest, 10);
+	ft_readtext(fd, &first_n, &rest);
+
+
+	return (ft_split_line(&rest, first_n));
 }
 
-char	*ft_readtext(int fd, int *first_n, char **rest)
+char    *ft_readtext(int fd, int *first_n, char **rest)
 {
 	char	*buffer;
 	char	*aux;
@@ -46,94 +40,41 @@ char	*ft_readtext(int fd, int *first_n, char **rest)
 
 	while (*first_n == -1)
 	{
-		buffer = malloc (sizeof(char) * (BUFFER_SIZE + 1));
+		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		if (!buffer)
 			return (0);
-		ft_bzero (buffer, BUFFER_SIZE + 1);
-		lenread = read (fd, buffer, BUFFER_SIZE);
-		if (lenread == -1)
-		{
-			free (buffer);
-			return (0);
-		}
-		if (lenread == 0 && !*rest)
-		{
-			free (buffer);
-			return (0);
-		}
-		if (lenread < BUFFER_SIZE)
-		{
-			*first_n = ft_strlen(buffer); //-1??
-		}
-	aux = ft_strjoin(*rest, buffer);
-	free(buffer);
-	if (*rest && ft_strlen(*rest) > 0)
-		free(*rest);
-	*rest = ft_strdup(aux);
-	free(aux);
-	if (lenread == BUFFER_SIZE)
-		*first_n = ft_strchr(*rest, 10);
-	if (lenread == 0)
-		first_n = ft_strlen(*rest);
-	return(*rest);
+		ft_bzero(buffer, BUFFER_SIZE + 1);
+		lenread = read(fd, buffer, BUFFER_SIZE);
+		aux = ft_strjoin(*rest, buffer);
+		free (buffer);
+		*rest = ft_strdup(aux);
+		*first_n = ft_strchr (*rest, 10);
+	}
+		return (*rest);
 }
 
-char	*ft_split_line(char **rest,int first_n)
+char    *ft_split_line(char **rest,int first_n)
 {
 	char	*line;
 	char	*aux;
 
 	line = ft_substr(*rest, 0, first_n + 1);
-	aux = ft_substr(*rest, first_n + 1, ft_strlen(*rest) - first_n - 1);
-	/*
-	//free(*rest); //si se libera pierde informacion
-	*rest = ft_strdup(aux);
-	free(aux);
-	*/
+	aux = ft_substr (*rest, first_n + 1, ft_strlen(*rest) - first_n - 1);
+
+	if (*rest && ft_strlen(*rest) > 0)
 	free (*rest);
 	*rest = aux;
-	free (aux);		//soluciona un leak
-	return(line);
+	free (aux);
+	return (line);	
 }
 
-void leakss()
+int main()
 {
-	system ("leaks a.out");
+	int	fd;
+
+	fd = open("textito.txt", O_RDONLY);
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	return (0);
 }
-
-
-int	main()
-
-{
-	int fd;
-	int i;
-
-	//atexit(leakss);
-
-	//fd = open("/home/josgarci/getnextline/41_no_nl.txt", O_RDONLY);
-	fd = open("5_no_nl", O_RDONLY);
-	if (LINEAS == 0)
-		while (get_next_line(fd));
-	else
-	{
-		i = 1;
-		while (i <= LINEAS)
-		{
-			printf("Linea %i->%s",i,get_next_line(fd));
-//get_next_line(fd);
-			i++;
-		}
-	}
-	return 0;
-}
-
-
-/*	**********PRINTS VARIOS:***************
-	printf("%s\n",);
-	printf("Buffer:\n%s\n",buffer);
-	printf("aux:\n%s\n",aux);
-	printf("rest:\n%s\n",rest);
-	printf("line:\n%s\n",line);
-	printf("%p\n",);
-*/
-
